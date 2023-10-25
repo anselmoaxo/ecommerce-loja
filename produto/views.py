d75b1eaac1192ea60d2d2811f7d3be01507e6656
Anselmo <anselmo.cstecnologia@gmail.com>
2023-10-25 11:58:07 -0300

    templates lista_produto , Detalhe produto

--- produto/views.py
+++ produto/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
+
+from .models import Produto
 
 
 class ListaProduto(ListView):
-    pass
+    model = Produto
+    template_name = "lista.html"
+    context_object_name = "lista_produtos"
+    paginate_by = 9
 
 
-class DetalheProduto(View):
-    pass
+class DetalheProduto(DetailView):
+    model = Produto
+    template_name = "detalhe.html"
+    context_object_name = "produto"
+    slug_url_kwarg = "slug"
 
 
 class AdicionarAoCarrinho(View):
