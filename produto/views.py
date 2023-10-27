9758b896b7c55dceb947b812ba2779b4d387bff4
Anselmo <anselmo.cstecnologia@gmail.com>
2023-10-27 00:59:49 -0300

    adicionando porduto no carrinho de compra e excluindo itens da compra , alteração no menu

--- produto/views.py
+++ produto/views.py
@@ -31,6 +31,25 @@ class AdicionarAoCarrinho(View):
             messages.error(self.request, "Produto não existe.")
             return redirect(http_referer)
         variacao = get_object_or_404(Variacao, id=variacao_id)
+        variacao_estoque = variacao.estoque
+        produto = variacao.produto
+
+        produto_id = produto.id
+        produto_nome = produto.nome
+        variacao_nome = variacao.nome or ""
+        preco_unitario = variacao.preco
+        preco_unitario_promocional = variacao.preco_promocional
+        quantidade = 1
+        slug = produto.slug
+        imagem = produto.imagem
+
+        if imagem:
+            imagem = imagem.name
+        else:
+            imagem = ""
+
+        if variacao.estoque < 1:
+            messages.error(self.request, "Saldo Insuficiente")
 
         if not self.request.session.get("carrinho"):
             self.request.session["carrinho"] = {}
@@ -39,18 +58,79 @@ class AdicionarAoCarrinho(View):
         carrinho = self.request.session["carrinho"]
 
         if variacao_id in carrinho:
-            pass
+            quantidade_carrinho = carrinho[variacao_id]["quantidade"]
+            quantidade_carrinho += 1
+
+            if variacao_estoque < quantidade_carrinho:
+                messages.warning(
+                    self.request,
+                    f"Saldo Insuficiente para {quantidade_carrinho}x"
+                    f"no produto {produto_nome}.Adicionamos"
+                    f"{variacao_estoque}x no seu carrinho.",
+                )
+                quantidade_carrinho = variacao_estoque
+            carrinho[variacao_id]["quantidade"] = quantidade_carrinho
+            carrinho[variacao_id]["preco_quantitativo"] = (
+                preco_unitario * quantidade_carrinho
+            )
+            carrinho[variacao_id]["preco_quantitativo_promocional"] = (
+                preco_unitario_promocional * quantidade_carrinho
+            )
+
         else:
-            pass
-        return HttpResponse(f"{variacao.produto} {variacao.nome}")
+            carrinho[variacao_id] = {
+                "produto_id": produto_id,
+                "produto_nome": produto_nome,
+                "variacao_nome": variacao.nome,
+                "variacao_id": variacao_id,
+                "preco_unitario": preco_unitario,
+                "preco_unitario_promocional": preco_unitario_promocional,
+                "preco_quantitativo": preco_unitario,
+                "preco_quantitativo_promocional": preco_unitario_promocional,
+                "quantidade": 1,
+                "slug": slug,
+                "imagem": imagem,
+            }
+        self.request.session.save()
+
+        messages.success(
+            self.request,
+            f"Produto {produto_nome} {variacao_nome} adicionado ao seu carrinho",
+        )
+
+        return redirect(http_referer)
 
 
 class RemoverDoCarrinho(View):
-    pass
+    def get(self, *args, **kwargs):
+        http_referer = self.request.META.get("HTTP_REFERER", reverse("produto:lista"))
+        variacao_id = self.request.GET.get("vid")
+
+        if not variacao_id:
+            return redirect(http_referer)
+        if not self.request.session.get("carrinho"):
+            return redirect(http_referer)
+
+        if variacao_id not in self.request.session["carrinho"]:
+            return redirect(http_referer)
+
+        carrinho = self.request.session["carrinho"][variacao_id]
+
+        messages.success(
+            self.request,
+            f' {carrinho["produto_nome"]} ({carrinho["variacao_nome"]})'
+            f" foi removido do seu carrinho.",
+        )
+
+        del self.request.session["carrinho"][variacao_id]
+        self.request.session.save()
+        return redirect(http_referer)
 
 
 class Carrinho(View):
-    pass
+    def get(self, *args, **kwargs):
+        context = {"carrinho": self.request.session.get("carrinho", {})}
+        return render(self.request, "carrinho.html", context)
 
 
 class Finalizar(View):
